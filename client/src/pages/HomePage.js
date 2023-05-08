88452c6c5559be426dbc4f3ddad7e7ea5d51d6f1
marielboher <mariellboheer@gmail.com>
2023-05-08 17:53:09 -0300

    add api jejeje

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,29 +7,31 @@ import { PostCard } from "../components/PostCard";
 export function HomePage() {
   const { posts } = usePosts();
 
-    const renderMain = () => {
-      if (posts.length === 0)
-      return (
-        <div className="flex flex-col jusify-center items-center">
-          <VscEmptyWindow className="w-48 h-48 text-white" />
-          <h1 className="text-white text-2xl">there are no posts</h1>
-        </div>
+  console.log(posts);
 
+  const renderMain = () => {
+    if (!Array.isArray(posts) || posts.length === 0)
+      return (
+        <div className="flex flex-col justify-center items-center">
+          <VscEmptyWindow className="w-48 h-48 text-white" />
+          <h1 className="text-white text-2xl">There are no posts</h1>
+        </div>
       );
-      return(
-        <div className="grid grid-cols-3 gap-4">
-        {posts?.map((post) => (
+
+    return (
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((post) => (
           <PostCard post={post} key={post._id} />
         ))}
       </div>
-      )
-     
-    }
+    );
+  };
+
   return (
     <div>
       <header className="flex justify-between py-4">
         <h1 className="text-2xl text-gray-300 font-bold">
-          Posts ({posts.length})
+          Posts ({Array.isArray(posts) ? posts.length : 0})
         </h1>
         <Link
           to="/new"
